9726c9bf0f40f9f676923dd89d32794d76f542fc
Nikunj <nikunj_sanghavi@persistent.co.in>
2016-04-26 19:40:14 +0530

    customized module
    stu3 support with basic read write and other interaction supported
    -fhir to swagger storing in individul files
    -added interactions as per requirement
    -added custom url support

--- index.js
+++ index.js
@@ -2,27 +2,33 @@ var Request = require('request');
 var Converter = require('./lib/conformance-to-swagger.js');
 
 module.exports = function(options, callback) {
-  callback = callback || function(err) {if (err) throw err};
-  var headers = {};
-  var auth = options.authorization;
-  if (auth) {
-    var authString = auth.username + ':' + auth.password;
-    authString = new Buffer(authString).toString('base64');
-    headers.Authorization = 'Basic ' + authString;
-  }
+	callback = callback || function(err) {
+		if (err)
+			throw err
+	};
+	var headers = {};
+	var auth = options.authorization;
+	if (auth) {
+		var authString = auth.username + ':' + auth.password;
+		authString = new Buffer(authString).toString('base64');
+		headers.Authorization = 'Basic ' + authString;
+	}
 
-  Request({
-    rejectUnauthorized: options.reject_unauthorized,
-    url: options.fhir_url + options.conformance_path,
-    headers: headers,
-    json: true,
-  }, function(err, resp, body) {
-    if (err) return callback(err);
-    var swagger = Converter.convert(options.fhir_url, body);
-    if (auth) {
-      swagger.securityDefinitions = swagger.securityDefinitions || {};
-      swagger.securityDefinitions.Basic = {type: 'basic'};
-    }
-    callback(null, swagger);
-  })
+	Request({
+		rejectUnauthorized : options.reject_unauthorized,
+		url : options.fhir_url + options.conformance_path,
+		headers : headers,
+		json : true,
+	}, function(err, resp, body) {
+		if (err)
+			return callback(err);
+		var swagger = Converter.convert(options, body);
+		if (auth) {
+			swagger.securityDefinitions = swagger.securityDefinitions || {};
+			swagger.securityDefinitions.Basic = {
+				type : 'basic'
+			};
+		}
+		callback(null, swagger);
+	})
 }
